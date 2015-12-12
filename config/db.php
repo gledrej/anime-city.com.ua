8ecd3031123531da8201d0a405feb06944dbe8c4
Gledrej <Gledrej1988@gmail.com>
2015-12-12 22:28:59 +0200

    yii

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=chat',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
-];
